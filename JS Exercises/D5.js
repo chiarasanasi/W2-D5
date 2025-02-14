fcdf8549cda8899a8a55b04d17cbfdcf3bd43867
chiarasanasi <chiarasanasi.work@gmail.com>
2025-02-14 16:15:45 +0100

    W2-D5

---
+++ JS Exercises/D5.js
@@ -0,0 +1,158 @@
+/*
+REGOLE
+- Tutte le risposte devono essere scritte in JavaScript
+- Puoi usare Google / StackOverflow ma solo quanto ritieni di aver bisogno di qualcosa che non è stato spiegato a lezione
+- Puoi testare il tuo codice in un file separato, o de-commentando un esercizio alla volta
+- Per visualizzare l'output, lancia il file HTML a cui è collegato e apri la console dagli strumenti di sviluppo del browser. 
+- Utilizza dei console.log() per testare le tue variabili e/o i risultati delle espressioni che stai creando.
+*/
+
+/* ESERCIZIO 1
+    Dato il seguente array, scrivi del codice per stampare ogni elemento dell'array in console.
+*/
+const pets = ["dog", "cat", "hamster", "redfish"]
+
+for (let i = 0; i < 4; i++) {
+  console.log(pets[i])
+}
+
+/* ESERCIZIO 2
+    Scrivi del codice per ordinare alfabeticamente gli elementi dell'array "pets".
+*/
+console.log(pets.sort())
+
+/* ESERCIZIO 3
+    Scrivi del codice per stampare nuovamente in console gli elementi dell'array "pets", questa volta in ordine invertito.
+*/
+
+pets.reverse()
+for (let i = 0; i < 4; i++) {
+  console.log(pets[i])
+}
+
+/* ESERCIZIO 4
+    Scrivi del codice per spostare il primo elemento dall'array "pets" in ultima posizione.
+*/
+console.log(pets) //per vedere com'è al momento l'array
+const first = pets.shift()
+pets.push(first)
+console.log(pets)
+
+/* ESERCIZIO 5
+    Dato il seguente array di oggetti, scrivi del codice per aggiungere ad ognuno di essi una proprietà "licensePlate" con valore a tua scelta.
+*/
+const cars = [
+  {
+    brand: "Ford",
+    model: "Fiesta",
+    color: "red",
+    trims: ["titanium", "st", "active"],
+  },
+  {
+    brand: "Peugeot",
+    model: "208",
+    color: "blue",
+    trims: ["allure", "GT"],
+  },
+  {
+    brand: "Volkswagen",
+    model: "Polo",
+    color: "black",
+    trims: ["life", "style", "r-line"],
+  },
+]
+const licensePlateValue = ["ciao", "wow", "tastiera"]
+for (let i = 0; i < cars.length; i++) {
+  cars[i].licensePlate = licensePlateValue[i]
+}
+console.log(cars)
+
+/* ESERCIZIO 6
+    Scrivi del codice per aggiungere un nuovo oggetto in ultima posizione nell'array "cars", rispettando la struttura degli altri elementi.
+    Successivamente, rimuovi l'ultimo elemento della proprietà "trims" da ogni auto.
+*/
+
+//nuovo oggetto
+const newCar = {
+  brand: "Renualt",
+  model: "Modus",
+  color: "yellow",
+  trims: ["titanium", "st", "active"],
+  licensePlate: "aaaaaa",
+}
+
+//aggiunto oggetto all'array cars
+cars.push(newCar)
+
+//rimozione ultimo elemento della prop. trims da ogni car
+for (let i = 0; i < cars.length; i++) {
+  cars[i].trims.pop()
+}
+console.log(cars)
+
+/* ESERCIZIO 7
+    Scrivi del codice per salvare il primo elemento della proprietà "trims" di ogni auto nel nuovo array "justTrims", sotto definito.
+*/
+let justTrims = []
+
+for (let i = 0; i < cars.length; i++) {
+  justTrims.push(cars[i].trims[0])
+}
+
+console.log(justTrims)
+
+/* ESERCIZIO 8
+    Cicla l'array "cars" e costruisci un if/else statament per mostrare due diversi messaggi in console. Se la prima lettera della proprietà
+    "color" ha valore "b", mostra in console "Fizz". Altrimenti, mostra in console "Buzz".
+*/
+for (let i = 0; i < cars.length; i++) {
+  if (cars[i].color.charAt(0) === "b") {
+    console.log("Fizz")
+  } else {
+    console.log("Buzz")
+  }
+}
+/* ESERCIZIO 9
+    Utilizza un ciclo while per stampare in console i valori del seguente array numerico fino al raggiungimento del numero 32.
+*/
+const numericArray = [
+  6, 90, 45, 75, 84, 98, 35, 74, 31, 2, 8, 23, 100, 32, 66, 313, 321, 105,
+]
+let apice = 0
+const numero = 0
+while (numericArray[apice] !== 32) {
+  console.log(numericArray[apice])
+  apice++
+}
+
+/* ESERCIZIO 10
+    Partendo dall'array fornito e utilizzando un costrutto switch, genera un nuovo array composto dalle posizioni di ogni carattere all'interno
+    dell'alfabeto italiano.
+    es. [f, b, e] --> [6, 2, 5]
+*/
+const charactersArray = ["g", "n", "u", "z", "d"]
+const numbersArray = []
+
+for (let i = 0; i < charactersArray.length; i++) {
+  let valore = charactersArray[i]
+  switch (valore) {
+    case "g":
+      numbersArray.push(7)
+      break
+    case "n":
+      numbersArray.push(12)
+      break
+    case "u":
+      numbersArray.push(19)
+      break
+    case "z":
+      numbersArray.push(21)
+      break
+    case "d":
+      numbersArray.push(4)
+      break
+    default:
+      console.log("non riconosco questo valore")
+  }
+}
+console.log(numbersArray)
